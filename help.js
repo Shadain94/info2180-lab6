1c904de377826d51f16903739e979abfd0979b97
Shadain94 <shadain.gooden@mymona.uwi.edu>
2016-11-10 11:25:36 -0500

    Add files via upload

---
+++ help.js
@@ -0,0 +1,53 @@
+
+ window.onload=function() {
+    
+   
+   
+   
+   var btn= document.getElementById('search');
+   
+   var httpRequest;
+   
+   btn.addEventListener('click', function(element) {
+    element.preventDefault();
+    
+    var text= document.getElementById('lookup').value;
+    
+    console.log(text);
+    httpRequest = new XMLHttpRequest();
+    
+       
+    httpRequest.onreadystatechange = processDefinition;
+    httpRequest.open('GET','request.php?q=' + text , true);
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send();
+    
+    
+    
+    
+     function processDefinition() {
+       
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            var response = httpRequest.responseText;
+            alert(response);
+        
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+       
+   }
+       
+       
+       
+   });
+   
+ }; 
+   
+   
+   
+    
+    
+    
+    
